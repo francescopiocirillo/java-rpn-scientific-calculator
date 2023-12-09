ca4237e4e87faf7a6bbe193515fd4d1e17284aca
apiantonio <apiantonio.aa@gmail.com>
2023-12-09 14:09:50 +0100

    commit 9-12

--- src/main/java/AH/gruppo14/stackbasedcalculator_gruppo14/StackView.java
+++ src/main/java/AH/gruppo14/stackbasedcalculator_gruppo14/StackView.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package AH.gruppo14.stackbasedcalculator_gruppo14_ref;
+package AH.gruppo14.stackbasedcalculator_gruppo14;
 
 import java.util.Stack;
 import javafx.geometry.Orientation;
